3734145372e02975c7b997ab767299ee7be26813
Sabyr Shatekov <sabyrsa@halykbank.kz>
2024-05-18 17:20:57 +0500

    added router /get-questions-level-of-sufficiency

--- candidate_assessment/routes.py
+++ candidate_assessment/routes.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
 
+from candidate_assessment import utils
+
 router = APIRouter()
 
 @router.get("/")
-def root():
+async def root():
     return {
-        "message": "Hello world!"
+        "message": "Hello!"
     }
+
+@router.get("/get-questions-level-of-sufficiency")
+async def get_questions_level_of_sufficiency():
+    return utils.Questions.get_level_of_sufficiency()
