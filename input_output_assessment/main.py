fa0d5b1f84236169a4efb01957052cc6fb6c8ab7
Шатеков Сабыр <00052497@nb>
2024-05-18 18:59:54 +0500

    feat input_output_assessment

--- input_output_assessment/main.py
+++ input_output_assessment/main.py
@@ -2,18 +2,12 @@ from input_output_assessment import utils
 from input_output_assessment import config
 
 def main():
-    answers = {}
-
-    for question, text in config.questions.items():
-        answer = input(f"{text} (А) да / (Б) нет: ")
-        while answer not in ['А', 'Б']:
-            print("Пожалуйста, введите 'А' для ответа 'да' или 'Б' для ответа 'нет'.")
-            answer = input(f"{text} (А) да / (Б) нет: ")
-        answers[question] = answer
-
-    motivation_score, conformity_score = utils.process_answers(answers)
-    motivation_level, conformity_level = utils.interpret_results(motivation_score, conformity_score)
-
+    motivation_answers = utils.get_answers(config.Questions.motivation_questions.value)
+    conformity_answers = utils.get_answers(config.Questions.conformity.value)
+    motivation_score = utils.get_score(answers=motivation_answers, scores=config.Scores.motivation.value)
+    conformity_score = utils.get_score(answers=conformity_answers, scores=config.Scores.conformity.value)
+    motivation_level = utils.get_level(score=motivation_score, levels=config.Level.motivation.value)
+    conformity_level = utils.get_level(score=conformity_score, levels=config.Level.conformity.value)
     print("Результаты теста:")
     print("Уровень мотивации к успеху:", motivation_level)
     print("Уровень независимости суждений и поведения:", conformity_level)
--- input_output_assessment/utils.py
+++ input_output_assessment/utils.py
@@ -1,47 +1,27 @@
-def process_answers(answers):
-    motivation_questions = [1, 4, 5, 7, 8]
-    conformity_questions = [11, 13, 16, 17, 20]
-    motivation_score = 0
-    conformity_score = 0
+from typing import Optional
 
+def get_answers(questions: dict) -> dict:
+    answers = dict()
+    for question, text in questions.items():
+        answer = input(f"{text} (А) да / (Б) нет: ")
+        while answer not in ['А', 'Б']:
+            print("Пожалуйста, введите 'А' для ответа 'да' или 'Б' для ответа 'нет'.")
+            answer = input(f"{text} (А) да / (Б) нет: ")
+        answers[question] = answer
+    return answers
+
+def get_score(answers: dict, scores: dict) -> int:
+    score = 0
     for question, answer in answers.items():
-        if question in motivation_questions:
-            if answer == 'A':
-                motivation_score += 1
-        elif question in conformity_questions:
-            if answer == 'B':
-                conformity_score += 1
+        if question in scores[answer]:
+            score += 1
+    return score
 
-    return motivation_score, conformity_score
 
-def interpret_results(motivation_score, conformity_score):
-    motivation_levels = {
-        10: "высокий уровень мотивации к успеху",
-        7: "уровень выше среднего мотивации к успеху",
-        5: "средний уровень мотивации к успеху",
-        3: "уровень ниже среднего мотивации к успеху",
-        0: "низкий уровень мотивации к успеху"
-    }
-
-    conformity_levels = {
-        10: "самостоятельность, независимость суждений и поведения",
-        7: "уровень выше среднего независимости суждений и поведения",
-        5: "средний уровень конформизма",
-        3: "уровень ниже среднего зависимости суждений и поведения от окружающих",
-        0: "выраженный конформизм, зависимость от мнения других"
-    }
-
-    motivation_level = None
-    conformity_level = None
-
-    for level, description in motivation_levels.items():
-        if motivation_score >= level:
-            motivation_level = description
+def get_level(score: int, levels: dict) -> Optional[str]:
+    result = None
+    for level, description in levels.items():
+        if score >= level:
+            result = description
             break
-
-    for level, description in conformity_levels.items():
-        if conformity_score >= level:
-            conformity_level = description
-            break
-
-    return motivation_level, conformity_level
+    return result
