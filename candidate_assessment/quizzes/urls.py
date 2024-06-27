e25dff39fb60a5005808f5310b4d44c9c99f0614
Sabyr Shatekov <shatekov.sabyr@gmail.com>
2024-06-28 00:08:59 +0500

    added dynamic urls

--- candidate_assessment/quizzes/urls.py
+++ candidate_assessment/quizzes/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from quizzes import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('types/<int:types_id>/', views.types),
+    path('types/<slug:types_slug>/', views.types_by_slug),
 ]
--- candidate_assessment/quizzes/views.py
+++ candidate_assessment/quizzes/views.py
@@ -3,3 +3,11 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse('Main page of quizzes')
+
+
+def types(request: HttpRequest, types_id: int) -> HttpResponse:
+    return HttpResponse(f'types: {types_id}')
+
+
+def types_by_slug(request: HttpRequest, types_slug: str) -> HttpResponse:
+    return HttpResponse(f'types slug: {types_slug}')
